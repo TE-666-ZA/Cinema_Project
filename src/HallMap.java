f2375d70590395b0fbd7e7af66f3b36aa5752d2a
Ustinya Petrova <ust.petrova@gmail.com>
2023-08-11 01:47:20 +0200

    feat: add methods createSession, chooseSeat, returnSeat, showSessionMap in HallMaps

--- src/HallMap.java
+++ src/HallMap.java
@@ -1,5 +1,33 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class HallMap {
-// мапы для кинозалов
 
+  // создаём класс, в котором будет храниться вся изначальная и обновлённая инфа о местах
+  private Map<String, List<List<String>>> hallSessions; // мапа для хранения информации о местах
 
+  public HallMap() {
+    hallSessions = new HashMap<>();
+  }
+
+  // метод для создания сеанса с местами и рядами
+  public void createSession(String sessionKey, List<List<String>> rows) {
+    hallSessions.put(sessionKey, rows);
+  }
+
+  // метод для того, чтобы выбрать купленное место
+  public void chooseSeat(String sessionKey, int row, int seatNumber) {
+
+  }
+
+  // метод для того, чтобы вернуть купленное место
+  public void returnSeat(String sessionKey, int row, int seatNumber) {
+
+  }
+
+  // метод для демонстрации карты мест
+  public void showSessionMap(String sessionKey) {
+
+  }
 }
