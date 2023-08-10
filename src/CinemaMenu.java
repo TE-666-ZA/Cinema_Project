8584b893c1ebe807e763d3a950d248a205646e84
Viktoriia Shcherbyna <silime7@gmail.com>
2023-08-10 18:14:19 +0200

    fix: add Enum values for mainMenu, and mainMenu in class

--- src/CinemaMenu.java
+++ src/CinemaMenu.java
@@ -1,4 +1,18 @@
 public class CinemaMenu {
-  //меню и проверки меню тут
 
+  //меню и проверки меню тут
+  public static void mainMenu() {
+
+    System.out.println(Separators.TIMETABLES.ordinal() + "." + Separators.TIMETABLES.getMessage());
+    System.out.println(Separators.FREE_SPACE.ordinal() + "." + Separators.FREE_SPACE.getMessage());
+    System.out.println(
+        Separators.BUYING_TICKETS.ordinal() + "." + Separators.BUYING_TICKETS.getMessage());
+    System.out.println(Separators.TIKETS_EXCHANGE_OR_RETURN.ordinal() + "."
+        + Separators.TIKETS_EXCHANGE_OR_RETURN.getMessage());
+    System.out.println(
+        Separators.ADMINISTRATOR.ordinal() + "." + Separators.ADMINISTRATOR.getMessage());
+    System.out.println(Separators.EXIT.ordinal() + "." + Separators.EXIT.getMessage());
+//    // separator();
+
+  }
 }
--- src/Separators.java
+++ src/Separators.java
@@ -1,3 +1,19 @@
 public enum Separators {
-  //разделители для файла
+  //разделители для файла  и константы меню
+  //пункты первого меню mainMenu
+  EXIT("Выход"),
+  TIMETABLES("Расписание"),
+  FREE_SPACE("Свободные места"),
+  BUYING_TICKETS("Покупка билетов"),
+  TIKETS_EXCHANGE_OR_RETURN("Обмен/Возврат билетов"),
+  ADMINISTRATOR("Администратор");
+  private final String message;
+
+  Separators(String message) {
+    this.message = message;
+  }
+
+  public String getMessage() {
+    return message;
+  }
 }
