153da69959e467bca95de340541b117d97319e9e
Viktoriia Shcherbyna <silime7@gmail.com>
2023-08-10 20:00:58 +0200

    fix: clear Separator, add classes FreeSpaceMenu in CinemaMain,

--- src/CinemaMenu.java
+++ src/CinemaMenu.java
@@ -1,5 +1,40 @@
 public class CinemaMenu {
 
+  enum EnumMainMenu {
+    //пункты первого меню mainMenu
+    EXIT("Выход"),
+    TIMETABLES("Расписание"),
+    FREE_SPACE("Свободные места"),
+    BUYING_TICKETS("Покупка билетов"),
+    TIKETS_EXCHANGE_OR_RETURN("Обмен/Возврат билетов"),
+    ADMINISTRATOR("Администратор");
+    private final String message;
+
+    EnumMainMenu(String message) {
+      this.message = message;
+    }
+
+    public String getMessage() {
+      return message;
+    }
+  }
+
+  enum FreeSpaceMenu {
+    //пункты меню Свободные места
+    RETURN_TO_THE_MAIN_MENU("Введите 0 чтоб вернуться в главное меню"),
+    CHANGE_DATE("Введите 1 чтоб изменить дату");
+    private final String messageFreeSpaceMenu;
+
+    FreeSpaceMenu(String messageFreeSpaceMenu) {
+      this.messageFreeSpaceMenu = messageFreeSpaceMenu;
+    }
+
+    public String getMessageFreeSpaceMenu() {
+      return messageFreeSpaceMenu;
+    }
+  }
+
+
   //меню и проверки меню тут
   // Метод для вывода разделительной строки между меню
   public static void separator() {
@@ -7,17 +42,23 @@ public class CinemaMenu {
   }
 
   public static void mainMenu() {
-
-    System.out.println(Separators.TIMETABLES.ordinal() + "." + Separators.TIMETABLES.getMessage());
-    System.out.println(Separators.FREE_SPACE.ordinal() + "." + Separators.FREE_SPACE.getMessage());
     System.out.println(
-        Separators.BUYING_TICKETS.ordinal() + "." + Separators.BUYING_TICKETS.getMessage());
-    System.out.println(Separators.TIKETS_EXCHANGE_OR_RETURN.ordinal() + "."
-        + Separators.TIKETS_EXCHANGE_OR_RETURN.getMessage());
+        EnumMainMenu.TIMETABLES.ordinal() + "." + EnumMainMenu.FREE_SPACE.getMessage());
     System.out.println(
-        Separators.ADMINISTRATOR.ordinal() + "." + Separators.ADMINISTRATOR.getMessage());
-    System.out.println(Separators.EXIT.ordinal() + "." + Separators.EXIT.getMessage());
+        EnumMainMenu.BUYING_TICKETS.ordinal() + "." + EnumMainMenu.BUYING_TICKETS.getMessage());
+    System.out.println(EnumMainMenu.TIKETS_EXCHANGE_OR_RETURN.ordinal() + "."
+        + EnumMainMenu.TIKETS_EXCHANGE_OR_RETURN.getMessage());
+    System.out.println(
+        EnumMainMenu.ADMINISTRATOR.ordinal() + "." + EnumMainMenu.ADMINISTRATOR.getMessage());
+    System.out.println(EnumMainMenu.EXIT.ordinal() + "." + EnumMainMenu.EXIT.getMessage());
     separator();
+  }
 
+  public static void FreeSpaceMenu() {
+    System.out.println(FreeSpaceMenu.CHANGE_DATE.ordinal() + "."
+        + FreeSpaceMenu.CHANGE_DATE.getMessageFreeSpaceMenu());
+    System.out.println(FreeSpaceMenu.RETURN_TO_THE_MAIN_MENU.ordinal() + "."
+        + FreeSpaceMenu.RETURN_TO_THE_MAIN_MENU.getMessageFreeSpaceMenu());
+    separator();
   }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("\u001B[32m" + "Добро пожаловать в Кинотеатр 'CINEMA'" + "\u001B[0m");
     CinemaMenu.mainMenu();
+    CinemaMenu.FreeSpaceMenu();
   }
 }
