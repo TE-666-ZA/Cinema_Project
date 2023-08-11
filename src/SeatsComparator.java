303b3b99384e60231c7200004ab1f37501bd964f
Ustinya Petrova <ust.petrova@gmail.com>
2023-08-11 18:15:22 +0200

    feat: add class SeatsComparator

---
+++ src/SeatsComparator.java
@@ -0,0 +1,9 @@
+import java.util.Comparator;
+
+public class SeatsComparator implements Comparator<Character[]> {
+
+  @Override
+  public int compare(Character[] o1, Character[] o2) {
+    return 0;
+  }
+}
