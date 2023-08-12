a25c63d92b695b6bb9bb85f171f382a90aa06348
Ustinya Petrova <ust.petrova@gmail.com>
2023-08-12 18:36:52 +0200

    feat: fill ComparatorForFilms

--- src/ComparatorForFilms.java
+++ src/ComparatorForFilms.java
@@ -1,3 +1,9 @@
-public class ComparatorForFilms {
+import java.util.Comparator;
 
+public class ComparatorForFilms implements Comparator<String> {
+
+  @Override
+  public int compare(String name1, String name2) {
+    return name1.compareToIgnoreCase(name2);
+  }
 }
