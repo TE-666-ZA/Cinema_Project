fa153233725904ea3602ec48767e50fef4b121d5
Ustinya Petrova <ust.petrova@gmail.com>
2023-08-12 18:27:58 +0200

    fix: delete SeatsComparator

--- src/SeatsComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class SeatsComparator implements Comparator<Character[]> {
-
-  @Override
-  public int compare(Character[] o1, Character[] o2) {
-    return 0;
-  }
-}
