6de9f6a8499e30f76eb58d4335a4e94850aedff2
Ustinya Petrova <ust.petrova@gmail.com>
2023-08-13 20:03:38 +0200

    fix!: delete ComparatorForDates and ComparatorForFilms

--- src/ComparatorForFilms.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class ComparatorForFilms implements Comparator<String> {
-
-  @Override
-  public int compare(String name1, String name2) {
-    return name1.compareToIgnoreCase(name2);
-  }
-}
