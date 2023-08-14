fe310192e8a94d17f6b7eb618c5db47f2ee1b6c3
Kenan Iusubovi <te666za@gmail.com>
2023-08-14 03:49:22 +0200

    feat: initial EnumInfoFullIndexes in Session class, rename readlall...., to readDate in Session class, rename readRest in FileEditor,

--- src/FileEditor.java
+++ src/FileEditor.java
@@ -1,20 +1,16 @@
 import java.io.*;
-import java.text.ParseException;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
 public class FileEditor {
+
     private final int KEY = 0;
     private final int VALUE = 1;
+    private final String READ_MAP_PREFIX = "|";
 
 
     private BufferedReader reader;
     private FileReader infoFull;
-
-
-
-
 
 
     FileEditor() throws IOException {
@@ -24,37 +20,37 @@ public class FileEditor {
 
     private String read(String prefix) throws IOException, NullPointerException {
         String input;
-      while((input = reader.readLine()) != null){
-          if(!input.trim().startsWith("#") && input.trim().startsWith(prefix)){
-              return input;
-          }
+        while ((input = reader.readLine()) != null) {
+            if (!input.trim().startsWith("#") && input.trim().startsWith(prefix)) {
+                return input;
+            }
         }
         return input;
     }
 
 
-    public LocalDate reeadDate() throws IOException{
+    public LocalDate reeadDate() throws IOException {
 
         return null;
     }
 
 
     public Map<Integer, Character[]> readMap(Map<Integer, Character[]> thisMap) throws IOException {
-    String[] temp = read("|").split(">");
-    temp[KEY] = temp[KEY].substring(1);
-    int key = Integer.parseInt(temp[KEY]);
-    char[] charArray = temp[VALUE].toCharArray();
-    Character[] value = new Character[charArray.length];
-    for (int i = 0; i < charArray.length; i++){
-        value[i] = charArray[i];
-    }
-    thisMap.put(key,value);
+        String[] temp = read(READ_MAP_PREFIX).split(">");
+        temp[KEY] = temp[KEY].substring(1);
+        int key = Integer.parseInt(temp[KEY]);
+        char[] charArray = temp[VALUE].toCharArray();
+        Character[] value = new Character[charArray.length];
+        for (int i = 0; i < charArray.length; i++) {
+            value[i] = charArray[i];
+        }
+        thisMap.put(key, value);
 
         return thisMap;
     }
 
-    public String readRest () throws IOException {
-        return read("&");
+    public String readData(String prefix) throws IOException {
+        return read(prefix);
     }
 
 
--- src/Session.java
+++ src/Session.java
@@ -1,21 +1,48 @@
 import java.io.IOException;
+import java.text.DateFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class Session {
+    static enum EnumInfoFullIndexes {
+
+        DATE_INDEX("/"),
+        TIME_INDEX("["),
+        TITLE_INDEX("&"),
+        BONUS_INDEX("*"),
+        SEPARATOR(",");
+
+
+        public final String messageEnumInfoFullIndexes;
+
+        private EnumInfoFullIndexes(String messageEnumInfoFullIndexes) {
+            this.messageEnumInfoFullIndexes = messageEnumInfoFullIndexes;
+        }
+
+        public String getMessageEnumInfoFullIndexes() {
+            return this.messageEnumInfoFullIndexes;
+        }
+    }
+
     FileEditor fileEditor;
-    private LocalDate[] date;
+    private LocalDate[] dates;
     private LocalTime[] time;
     private String[] title;
     private String[] bonus;
-    String content;
+
 
     Session() throws IOException {
         fileEditor = new FileEditor();
-        generateAllSessionData();
+        readDate(EnumInfoFullIndexes.DATE_INDEX.getMessageEnumInfoFullIndexes(), EnumInfoFullIndexes.SEPARATOR.getMessageEnumInfoFullIndexes());
     }
 
-    public void generateAllSessionData() throws IOException {
-       content  = fileEditor.readRest();
+    public void readDate(String prefix, String separator) throws IOException {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yy");
+        String[] temp = fileEditor.readData(prefix).split(separator);
+        this.dates = new LocalDate[temp.length];
+        for (int i = 0; i < temp.length; i++) {
+            dates[i] = LocalDate.parse(temp[i], formatter);
+        }
     }
 }
