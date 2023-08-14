50e70f841d8650af3d5c6f854615fab32667f617
Kenan Iusubovi <te666za@gmail.com>
2023-08-14 09:32:43 +0200

    fix FileEditor for tests

--- src/FileEditor.java
+++ src/FileEditor.java
@@ -6,33 +6,32 @@ public class FileEditor {
     private final int KEY = 0;
     private final int VALUE = 1;
     private final String MAP_PREFIX = "|";
+    private final String MAP_SPLITTER = "";
     private final String MAP_KEY_VALUE_SPLITTER = ">";
     private final int REMOVE_FIRST_INDEX = 1;
 
-    private BufferedReader reader;
-    private FileReader input;
-    private BufferedWriter writer;
-    private FileWriter output;
+     private int startLine;
 
-
-    FileEditor() throws IOException {
-        this.input = new FileReader("res/InfoFull.txt");
-        this.reader = new BufferedReader(input);
-
+    public FileEditor() {
+        this.startLine = 0;
     }
 
-    private String read(String prefix) throws IOException, NullPointerException {
-        String input;
+    private String read(String prefix , String splitter) throws IOException, NullPointerException {
+        FileReader in = new FileReader("res/InfoFull.txt");
+        BufferedReader reader = new BufferedReader(in);
+        StringBuilder result = new StringBuilder();
+            String input;
         while ((input = reader.readLine()) != null) {
-            while (input.trim().startsWith(prefix)) {
-                return input;
+            if (input.trim().startsWith(prefix)) {
+                result.append(input.substring(REMOVE_FIRST_INDEX)).append(splitter);
             }
         }
-        return input;
+        reader.close();
+        return result.toString();
     }
 
     public Map<Integer, Character[]> readMap(Map<Integer, Character[]> thisMap) throws IOException {
-        String[] temp = read(MAP_PREFIX).split(MAP_KEY_VALUE_SPLITTER);
+        String[] temp = read(MAP_PREFIX,MAP_SPLITTER).split(MAP_KEY_VALUE_SPLITTER);
         temp[KEY] = temp[KEY].substring(REMOVE_FIRST_INDEX);
         int key = Integer.parseInt(temp[KEY]);
         char[] charArray = temp[VALUE].toCharArray();
@@ -45,30 +44,40 @@ public class FileEditor {
         return thisMap;
     }
 
-    public String readData(String prefix) throws IOException {
-        return read(prefix).substring(REMOVE_FIRST_INDEX);
-    }
-
-    public void close() throws IOException {
-        reader.close();
+    public String readData(String prefix,String splitter) throws IOException {
+        return read(prefix,splitter);
     }
 
     public void write(String[] data, String prefix) throws IOException {
-        this.output = new FileWriter("res/InfoFull.txt");
-        this.writer = new BufferedWriter(output);
+        FileWriter out= new FileWriter("res/InfoFull.txt");
+         BufferedWriter writer = new BufferedWriter(out);
+        FileReader in = new FileReader("res/InfoFull.txt");
+        BufferedReader reader = new BufferedReader(in);
+        String temp;
+        int currentLine = 0;
+           boolean writeSection = false;
 
-        for (String output : data) {
-            writer.write(prefix + output);
-            writer.newLine();
-        }
+           while ((temp = reader.readLine()) != null){
+                   currentLine++;
+                if(currentLine >= startLine ){
+                    startLine = currentLine;
+                    writeSection = true;
+                }
+                if(writeSection) {
+                    for (String output : data) {
+                        writer.write(prefix + output);
+                        writer.newLine();
+                    }
+                }
+           }
         writer.close();
     }
 
-    public void writeMap(Map<Integer, Character[]> map, String prefix) throws IOException {
-        this.output = new FileWriter("res/InfoFull.txt");
-        this.writer = new BufferedWriter(output);
+    public void writeMap(Map<Integer, Character[]> map) throws IOException {
+        FileWriter output = new FileWriter("res/InfoFull.txt");
+        BufferedWriter writer = new BufferedWriter(output);
         for (Map.Entry<Integer, Character[]> thisMap : map.entrySet()) {
-            writer.write(prefix + thisMap.getKey() + MAP_KEY_VALUE_SPLITTER);
+            writer.write(MAP_PREFIX + thisMap.getKey() + MAP_KEY_VALUE_SPLITTER);
             Character[] temp = thisMap.getValue();
             for (Character value : temp) {
                 writer.write(value);
