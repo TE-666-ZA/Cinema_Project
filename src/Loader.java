5660472f505efe469a9b40b7af90a509990cd8d2
Kenan Iusubovi <te666za@gmail.com>
2023-08-14 06:40:21 +0200

    fix: rename FileEditor to Loader, remove writing logic.

--- src/Loader.java
+++ src/Loader.java
@@ -1,33 +1,29 @@
 import java.io.*;
 import java.util.Map;
 
-public class FileEditor {
+public class Loader {
 
     private final int KEY = 0;
     private final int VALUE = 1;
     private final String MAP_PREFIX = "|";
-    private final String COMMENT_PREFIX = "#";
+    private final String MAP_KEY_PREFIX = ">";
     private final int REMOVE_FIRST_INDEX = 1;
 
 
     private BufferedReader reader;
     private FileReader input;
-    private BufferedWriter writer;
-    private FileWriter output;
 
 
-    FileEditor() throws IOException {
+
+    Loader() throws IOException {
         this.input = new FileReader("res/InfoFull.txt");
         this.reader = new BufferedReader(input);
-        this.output = new FileWriter("res/InfoFull.txt");
-        this.writer = new BufferedWriter(output);
-
-    }
+           }
 
     private String read(String prefix) throws IOException, NullPointerException {
         String input;
         while ((input = reader.readLine()) != null) {
-            if (!input.trim().startsWith(COMMENT_PREFIX) && input.trim().startsWith(prefix)) {
+            if (input.trim().startsWith(prefix)) {
                 return input;
             }
         }
@@ -35,7 +31,7 @@ public class FileEditor {
     }
 
     public Map<Integer, Character[]> readMap(Map<Integer, Character[]> thisMap) throws IOException {
-        String[] temp = read(MAP_PREFIX).split(">");
+        String[] temp = read(MAP_PREFIX).split(MAP_KEY_PREFIX);
         temp[KEY] = temp[KEY].substring(REMOVE_FIRST_INDEX);
         int key = Integer.parseInt(temp[KEY]);
         char[] charArray = temp[VALUE].toCharArray();
@@ -52,15 +48,8 @@ public class FileEditor {
         return read(prefix).substring(REMOVE_FIRST_INDEX);
     }
 
-    public void close() throws IOException {
+    public void closeReader() throws IOException {
         reader.close();
     }
-    public void write(String[] data, String prefix) throws IOException {
-        for(String output : data) {
-            if(!output.trim().startsWith(COMMENT_PREFIX) && output.trim().startsWith(prefix)){
-                writer.write(output);
-                writer.newLine();
-            }
-        }
-    }
+
 }
