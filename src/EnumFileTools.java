9e19865ba8e57b7c6350d74305f8bd1b71d37cd8
Ustinya Petrova <ust.petrova@gmail.com>
2023-09-12 17:48:24 +0200

    fix: improve docstring comments

--- src/EnumFileTools.java
+++ src/EnumFileTools.java
@@ -1,3 +1,6 @@
+/**
+ * Enum для хранения различных индексов и разделителей, используемых при работе с файлами
+ */
 public enum EnumFileTools {
     DATE_INDEX("/"),
     TIME_INDEX("["),
@@ -10,12 +13,23 @@ public enum EnumFileTools {
     MAP_KEY_VALUE_SPLITTER(">"),
     SPLITTER(",");
 
+    // Константа, которая хранит текстовое представление элемента
     private final String messageEnumFileTools;
 
+    /**
+     * Конструктор с заданием текстового представления элемента
+     *
+     * @param messageEnumFileTools текстовое представление элемента
+     */
     EnumFileTools(String messageEnumFileTools) {
         this.messageEnumFileTools = messageEnumFileTools;
     }
 
+    /**
+     * Геттер для получения текстового представления элемента
+     *
+     * @return текстовое представление элемента
+     */
     public String getTool() {
         return this.messageEnumFileTools;
     }
