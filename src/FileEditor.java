270baa306e3bafaa245481d16727b4044ab38d97
Ustinya Petrova <ust.petrova@gmail.com>
2023-09-12 19:01:01 +0200

    feat: add docstring comments

--- src/FileEditor.java
+++ src/FileEditor.java
@@ -1,21 +1,41 @@
 import java.io.*;
 import java.util.Map;
 
+/**
+ * Класс для работы с файлами, в которых хранится информация о сеансах и билетах в кинозале.
+ * Этот класс обеспечивает чтение и запись/перезапись данных из/в файлы.
+ */
 public class FileEditor {
 
+  // Индекс ключа (части строки) при разборе данных из файла
   private final int KEY = 0;
+  // Индекс значения (части строки) при разборе данных из файла
   private final int VALUE = 1;
+  // Индекс для удаления первого символа в строке (нулевой индекс)
   private final int REMOVE_FIRST_INDEX = 1;
+  // Максимальная длина строки в файле "InfoFull.txt"
   private final int INFO_FULL_LENGHT = 48;
+  // Поток для чтения данных из файла
   private BufferedReader reader;
+  // Объект для чтения данных из файла
   private FileReader in;
+  // Объект для записи данных в файл
   private FileWriter out;
+  // Поток для записи данных в файл
   private BufferedWriter writer;
+  // Переменная, содержащая путь к файлу "InfoFull.txt"
   private String fileAllData;
+  // Переменная, содержащая путь к файлу "Check.txt"
   private String fileCheque;
-  private int lines; // считает кол-во строк в FullInfo
+  // Количество строк в файле "InfoFull.txt"
+  private int lines;
+  // Счетчик для отслеживания количества считанных записей из "InfoFull.txt"
   private int halMapReaderCounter;
 
+  /**
+   * Конструктор класса
+   * Инициализирует объект FileEditor и задает пути к файлам
+   */
   public FileEditor() {
     this.fileAllData = "res/InfoFull.txt";
     this.fileCheque = "res/Check.txt";
@@ -23,11 +43,17 @@ public class FileEditor {
     this.halMapReaderCounter = 0;
   }
 
+  /**
+   * Метод читает данные из файла на основе указанного префикса и разделителя
+   *
+   * @param prefix префикс, используемый для поиска соответствующих данных в файле
+   * @param splitter разделитель для объединения данных в строку
+   * @return строка, содержащая считанные данные из файла, объединенные разделителем
+   */
   private String read(String prefix,
-      String splitter) { // эту функцию не трогать не для ваших грязныйх ручишек !!!
+      String splitter) {
     boolean isCheque = true;
     StringBuilder result = new StringBuilder();
-    int mapCounter = 0;
     String input;
     try {
       if (prefix != EnumFileTools.CHEQUE_INDEX.getTool()) {
@@ -57,8 +83,14 @@ public class FileEditor {
     return null;
   }
 
+  /**
+   * Метод для чтения данных из файла и заполнения ими структуры данных Map
+   *
+   * @param thisMap структура данных Map для заполнения считанными данными
+   * @return заполненная структура данных Map
+   */
   public Map<Integer, Character[]> readMap(
-      Map<Integer, Character[]> thisMap) { // эта функция читает и заполняет мапу предназначен для использования в классе HALLMAP
+      Map<Integer, Character[]> thisMap) {
     try {
       if (halMapReaderCounter == 0) {
         this.in = new FileReader(fileAllData);
@@ -85,19 +117,31 @@ public class FileEditor {
         }
       }
     } catch (IOException | NullPointerException e) {
-      System.err.println("Файл для чтения  не найден! Обратитесь к сервисному руководству ");
+      System.err.println("Файл для чтения не найден! Обратитесь к сервисному руководству ");
     }
     return null;
   }
 
-
-  public String readData(String prefix,
-      String splitter) { // этот метод считывает дату по переданным значениям предназначен для класса Session
+  /**
+   * Метод для чтения данных из файла по заданным параметрам
+   *
+   * @param prefix префикс для поиска данных в файле
+   * @param splitter разделитель между данными
+   * @return считанные данные из файла
+   */
+    public String readData(String prefix,
+      String splitter) {
     return read(prefix, splitter);
   }
 
+  /**
+   * Метод для записи данных в файлы по заданным параметрам.
+   *
+   * @param data данные для записи
+   * @param prefix префикс для определения файла, в который будет произведена запись
+   */
   public void writeData(String data,
-      String prefix) { // этот метод записывает файлы по заданным параметрам предназначен для класса Session
+      String prefix) {
     boolean isCheque = true;
     try {
       if (prefix != EnumFileTools.CHEQUE_INDEX.getTool()) {
@@ -120,6 +164,11 @@ public class FileEditor {
     }
   }
 
+  /**
+   * Метод возвращает количество строк в файле
+   *
+   * @return количество строк в файле
+   */
   private int getLines() {
     try {
       this.in = new FileReader(fileAllData);
@@ -133,22 +182,32 @@ public class FileEditor {
       in.close();
       return lines;
     } catch (IOException e) {
-
+      System.out.println("Ошибка чтения!");
     }
     return 0;
   }
 
-
+  /**
+   * Метод используется только внутри класса.
+   * Он нужен для того, чтобы перезаписывать значения в файл FullInfo, а не создавать новые файлы.
+   *
+   * @throws IOException
+   */
   private void resetFile()
-      throws IOException { //этот метод только внутреклассовый он нужен для того чтобы перезаписывать значения в FullInfo а не создавать новые по тз
+      throws IOException {
     writer.close();
     out.close();
     this.out = new FileWriter(fileAllData, false);
     this.writer = new BufferedWriter(out);
   }
 
+  /**
+   * Метод для записи данных Map в файл для хранения информации о местах в кинозале
+   *
+   * @param map структура данных Map для записи.
+   */
   public void writeMap(
-      Map<Integer, Character[]> map) { // этот метод записывает карту в файл FullInfo предназначен для класса HallMap
+      Map<Integer, Character[]> map) {
     try {
       this.out = new FileWriter(fileAllData, true);
       this.writer = new BufferedWriter(out);
